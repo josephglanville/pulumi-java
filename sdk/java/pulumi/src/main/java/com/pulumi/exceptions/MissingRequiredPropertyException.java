e62b275f648a012a9b7551f32ba5f1eb16a93143
Zaid Ajaj <zaid.naom@gmail.com>
2023-09-27 17:59:22 +0200

    MissingRequiredPropertyException should be a RuntimeException

--- sdk/java/pulumi/src/main/java/com/pulumi/exceptions/MissingRequiredPropertyException.java
+++ sdk/java/pulumi/src/main/java/com/pulumi/exceptions/MissingRequiredPropertyException.java
@@ -3,7 +3,7 @@ package com.pulumi.exceptions;
 /**
  * MissingRequiredPropertyException is thrown when a required input property is missing from a resource args builder
  */
-public class MissingRequiredPropertyException extends Exception {
+public class MissingRequiredPropertyException extends RuntimeException {
     public MissingRequiredPropertyException(String builderName, String propertyName) {
         super("Missing required property '" + propertyName + " when constructing '" + builderName + "'");
     }
